c7ef0b50be2ce0a8910ce03213a51489088f2466
jk <josesitoelalto@gmail.com>
2026-04-08 23:07:41 -0600

    feat: Add users model

---
+++ src/models/Users.model.ts
@@ -0,0 +1,62 @@
+import { CreationOptional, DataTypes, Model, Optional } from "sequelize";
+import Database from "../config/db.js";
+
+type UserAttributes = {
+    user_id: number;
+    uuid: string;
+    name: string;
+    email: string;
+    pass: string;
+    image: string | null;
+}
+
+type UserCreationAttributes = Optional<UserAttributes, 'user_id' | 'uuid'>;
+
+
+export class User extends Model<UserAttributes, UserCreationAttributes> {
+    declare user_id: CreationOptional<number>;
+    declare uuid: CreationOptional<string>;
+    declare name: string;
+    declare email: string;
+    declare pass: string;
+    declare image: string | null;
+    declare created_at: CreationOptional<Date>;
+    declare updated_at: CreationOptional<Date>;
+}
+
+User.init(
+    {
+        user_id: {
+            type:DataTypes.INTEGER,
+            primaryKey:true,
+            autoIncrement:true
+        },
+        uuid: {
+            type:DataTypes.UUID,
+            defaultValue:DataTypes.UUIDV4,
+            unique:true,
+            allowNull:false
+        },
+        name: {
+            type:DataTypes.STRING,
+            allowNull:false
+        },
+        email: {
+            type:DataTypes.STRING,
+            allowNull:false
+        },
+
+        pass: {
+            type:DataTypes.STRING,
+            allowNull:false
+        },
+        image: {
+            type:DataTypes.STRING,
+            allowNull:true
+        }
+    },
+    {
+        tableName:'users',
+        sequelize:Database.db
+    }
+)
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,8 +1,10 @@
+import { User } from "./Users.model.js";
 import { Presentation } from "./Presentation.model.js";
 import { Flavor } from "./Flavor.model.js";
 
 
 export {
+    User,
     Presentation,
     Flavor
 };
