a2206b2c5a12de904ea85e5e3e78818cf8c09288
jk <josesitoelalto@gmail.com>
2026-04-10 13:03:49 -0600

    refactor: Define function to create a new user

--- src/services/PresentationService.ts
+++ src/services/PresentationService.ts
@@ -4,7 +4,7 @@ class PresentationService {
     static async createPresentation(data: any) {
         // Aquí podrías validar si ya existe una presentación con el mismo nombre
         // o aplicar lógica específica de la envasadora antes de guardar.
-        return await Presentation.create(data);
+        await Presentation.create(data);
     }
 
     static async getAllPresentations() {
---
+++ src/services/UserService.ts
@@ -0,0 +1,31 @@
+import { Op } from "sequelize";
+import bcrypt from "bcrypt";
+import { User, UserCreationAttributes } from "../models/Users.model.js";
+import { DuplicateError } from "../errors/SpecificErrors.js";
+
+export class UserService {
+    static async createUser(data: UserCreationAttributes) {
+        const { email, pass } = data;
+        const userExistst = await User.findOne({
+            where: {
+                email: {
+                    [Op.eq]: email
+                }
+            }
+        });
+
+        if (userExistst) {
+            throw new DuplicateError("El correo ya esta registrado en esta app.");
+        }
+
+        const saltRounds = 12;
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashedPass = bcrypt.hashSync(pass, salt);
+
+        let nwUser = new User(data);
+        nwUser.pass = hashedPass;
+
+        return nwUser;
+
+    }
+}
