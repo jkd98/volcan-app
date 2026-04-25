7fd8e1875a63da9600ddfa899bd8b0201706e1da
jk <josesitoelalto@gmail.com>
2026-04-24 20:46:35 -0600

    fix: Correct userService import and usage in createUser function

--- src/controllers/User.Controller.ts
+++ src/controllers/User.Controller.ts
@@ -1,12 +1,12 @@
-import { User } from "../models/Users.model.js";
-import { UserService } from "../services/UserService.js";
 import { NextFunction, Request, Response } from "express";
-import { IApiResponse } from "../interfaces/IIApiResponse.js";
-import { AppError } from "../errors/AppError.js";
+
+import { User } from "../models/Users.model.js";
+import userService from "../services/UserService.js"
+import { IApiResponse } from "../interfaces/IApiResponse.js";
 
 export const createUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const result = await UserService.createUser(req.body);
+        const result = await userService.createUser(req.body);
         const response: IApiResponse<User> = {
             success: true,
             message: "Usuario registrado",
